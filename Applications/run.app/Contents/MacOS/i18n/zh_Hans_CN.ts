1fa4601b75ccfaa7511ae82cf82ab14199d4f89b
杨震勋 <2210067895@qq.com>
2024-08-19 07:55:01 +0800

    Update.

---
+++ Applications/run.app/Contents/MacOS/i18n/zh_Hans_CN.ts
@@ -0,0 +1,316 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en_GB">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui_NoDone.py" line="172"/>
+        <source>Simple PPT</source>
+        <translation>Simple-PPT</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="173"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="174"/>
+        <source>Feedback</source>
+        <translation>反馈</translation>
+    </message>
+    <message>
+        <source>checkupdate</source>
+        <translation type="vanished">检查更新</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="175"/>
+        <source>Check Update</source>
+        <translation>检查更新</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="176"/>
+        <source>Quit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="177"/>
+        <source>English(UK)</source>
+        <translation>English(UK)</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="178"/>
+        <source>u4e2du6587uff08u7b80u4f53uff09</source>
+        <translation>\u4e2d\u6587\uff08\u7b80\u4f53\uff09</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="179"/>
+        <source>u4e2du6587uff08u7e41u9ad4uff09</source>
+        <translation>\u4e2d\u6587\uff08\u7e41\u9ad4\uff09</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="180"/>
+        <source>Function</source>
+        <translation>功能</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="181"/>
+        <location filename="../ui_NoDone.py" line="182"/>
+        <source>Creat PPT</source>
+        <translation>生成PPT</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="183"/>
+        <source>format</source>
+        <translation>格式</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="184"/>
+        <source>pptx</source>
+        <translation>pptx</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="186"/>
+        <source>title page</source>
+        <translation>标题页面</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="187"/>
+        <source>title text</source>
+        <translation>标题文字</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="188"/>
+        <source>subtitle text</source>
+        <translation>副标题文字</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="189"/>
+        <source>Style</source>
+        <translation>风格</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="190"/>
+        <location filename="../ui_NoDone.py" line="206"/>
+        <source>Title Slide</source>
+        <translation>标题幻灯片</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="191"/>
+        <location filename="../ui_NoDone.py" line="207"/>
+        <source>Title and Content</source>
+        <translation>标题和内容</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="192"/>
+        <location filename="../ui_NoDone.py" line="208"/>
+        <source>Section Header</source>
+        <translation>节标题</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="193"/>
+        <location filename="../ui_NoDone.py" line="209"/>
+        <source>Two Content</source>
+        <translation>两栏内容</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="194"/>
+        <location filename="../ui_NoDone.py" line="210"/>
+        <source>Comparison</source>
+        <translation>比较</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="195"/>
+        <location filename="../ui_NoDone.py" line="211"/>
+        <source>Title Only</source>
+        <translation>仅标题</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="196"/>
+        <location filename="../ui_NoDone.py" line="212"/>
+        <source>Blank</source>
+        <translation>空白</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="197"/>
+        <location filename="../ui_NoDone.py" line="213"/>
+        <source>Content with Caption</source>
+        <translation>内容与标题</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="198"/>
+        <location filename="../ui_NoDone.py" line="214"/>
+        <source>Pictrue with Caption</source>
+        <translation>图片与标题</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="199"/>
+        <location filename="../ui_NoDone.py" line="215"/>
+        <source>Title and vertical text</source>
+        <translation>标题与竖排文字</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="200"/>
+        <location filename="../ui_NoDone.py" line="216"/>
+        <source>Vertical headings and text</source>
+        <translation>竖排标题与文本</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="202"/>
+        <source>text(.docx file or markdown)</source>
+        <translation>文本文件（.docx或markdown）</translation>
+    </message>
+    <message>
+        <source>Change file</source>
+        <translation type="vanished">选择文件</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="203"/>
+        <source>Chage file</source>
+        <translation>选择文件</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="204"/>
+        <source>Creat</source>
+        <translation>创造</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="205"/>
+        <source>text style</source>
+        <translation>正文风格</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="218"/>
+        <source>Help</source>
+        <translation>帮助</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="219"/>
+        <source>Edit</source>
+        <translation>编辑</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="220"/>
+        <source>Language</source>
+        <translation>语言</translation>
+    </message>
+</context>
+<context>
+    <name>MessageBox</name>
+    <message>
+        <location filename="../run.py" line="43"/>
+        <location filename="../run.py" line="65"/>
+        <location filename="../run.py" line="83"/>
+        <location filename="../run.py" line="123"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="43"/>
+        <location filename="../run.py" line="123"/>
+        <source>Error:&lt;br/&gt;%s&lt;br/&gt;&lt;br/&gt;&lt;a href=https://github.com/Simple-PPT/Simple-PPT/issues/new&gt;Feedback Error&lt;/a&gt;</source>
+        <translation>错误：&lt;br/&gt;%s&lt;br/&gt;&lt;br/&gt;&lt;a href=https://github.com/Simple-PPT/Simple-PPT/issues/new&gt;反馈错误&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="46"/>
+        <source>New version</source>
+        <translation>新版本</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="46"/>
+        <source>Find a new version:%s.
+Would you like to update?</source>
+        <translation>找到了一个新版本：%s。
+你想要升级吗？</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="53"/>
+        <location filename="../run.py" line="71"/>
+        <source>Download time out</source>
+        <translation>下载超时</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="53"/>
+        <location filename="../run.py" line="71"/>
+        <source>Download time out.&lt;/br&gt;Please try again.</source>
+        <translation>下载超时。&lt;br/&gt;请再试一次。</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="65"/>
+        <location filename="../run.py" line="83"/>
+        <source>Error:&lt;br/&gt;We don&apos;t know why, error code %s.&lt;br/&gt;&lt;br/&gt;&lt;a href=https://github.com/Simple-PPT/Simple-PPT/issues/new&gt;Feedback Error&lt;/a&gt;</source>
+        <translation>错误:&lt;br/&gt;我们不知道为什么, 错误代码 %s.&lt;br/&gt;&lt;br/&gt;&lt;a href=https://github.com/Simple-PPT/Simple-PPT/issues/new&gt;反馈错误&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="85"/>
+        <source>No new version</source>
+        <translation>没有新版本</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="85"/>
+        <source>Your version is latest.</source>
+        <translation>您的版本是最新的。</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="102"/>
+        <source>wrong file path</source>
+        <translation>错误的文件路径</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="102"/>
+        <source>The file&apos;s path is wrong!</source>
+        <translation>这个文件的路径是错的！</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="121"/>
+        <source>PPT ready</source>
+        <translation>PPT准备好了</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="121"/>
+        <source>Your PPT is ready!
+It&apos;s in %s/ppts/%s.pptx</source>
+        <translation>您的PPT制作好了！
+它在%s/ppts/%s</translation>
+    </message>
+    <message>
+        <source>Your PPT is ready!
+It&apos;s in %s/ppts/%s</source>
+        <translation type="vanished">您的PPT制作好了！
+它在%s/ppts/%s</translation>
+    </message>
+    <message>
+        <source>Your PPT is ready!</source>
+        <translation type="vanished">您的PPT制作好了！</translation>
+    </message>
+</context>
+<context>
+    <name>QFileDialog</name>
+    <message>
+        <location filename="../run.py" line="87"/>
+        <source>Open a file</source>
+        <translation>打开一个文件</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="87"/>
+        <source>Documentation(*.docx *.md)</source>
+        <translation>文档 (*.docx *.md)</translation>
+    </message>
+</context>
+<context>
+    <name>about</name>
+    <message>
+        <location filename="../run.py" line="31"/>
+        <source>&lt;h2&gt;Simple PPT&lt;/h2&gt;version:0.0.2&lt;br/&gt;It can make a PPT for you.&lt;br/&gt;More see:&lt;a href=https://github.com/Simple-PPT/Simple-PPT&gt;Github&lt;/a&gt;</source>
+        <translation>&lt;h2&gt;Simple PPT&lt;/h2&gt;版本:0.0.2&lt;br/&gt;它可以为您做一个PPT。&lt;br/&gt;更多请见：&lt;a href=https://github.com/Simple-PPT/Simple-PPT&gt;Github&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="31"/>
+        <source>About Simple PPT 0.1</source>
+        <translation>关于Simple PPT 0.1</translation>
+    </message>
+    <message>
+        <source>About Simple PPT 0.0.2</source>
+        <translation type="vanished">关于Simple PPT 0.1</translation>
+    </message>
+</context>
+</TS>
---
+++ Applications/run.app/Contents/MacOS/i18n/zh_Hant_CN.ts
@@ -0,0 +1,283 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW" sourcelanguage="en_GB">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui_NoDone.py" line="172"/>
+        <source>Simple PPT</source>
+        <translation>Simple PPT</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="173"/>
+        <source>About</source>
+        <translation>大約</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="174"/>
+        <source>Feedback</source>
+        <translation>反饋</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="175"/>
+        <source>checkupdate</source>
+        <translation>檢查更新</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="176"/>
+        <source>Quit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="180"/>
+        <source>Function</source>
+        <translation>功能</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="181"/>
+        <location filename="../ui_NoDone.py" line="182"/>
+        <source>Creat PPT</source>
+        <translation>創造PPT</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="183"/>
+        <source>format</source>
+        <translation>格式</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="184"/>
+        <source>pptx</source>
+        <translation>pptx</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="186"/>
+        <source>title page</source>
+        <translation>扉頁</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="187"/>
+        <source>title text</source>
+        <translation>標題文本</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="188"/>
+        <source>subtitle text</source>
+        <translation>字幕文本</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="189"/>
+        <source>Style</source>
+        <translation>風格</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="190"/>
+        <location filename="../ui_NoDone.py" line="206"/>
+        <source>Title Slide</source>
+        <translation>標題幻燈片</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="191"/>
+        <location filename="../ui_NoDone.py" line="207"/>
+        <source>Title and Content</source>
+        <translation>標題和內容</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="192"/>
+        <location filename="../ui_NoDone.py" line="208"/>
+        <source>Section Header</source>
+        <translation>節標題</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="193"/>
+        <location filename="../ui_NoDone.py" line="209"/>
+        <source>Two Content</source>
+        <translation>兩個內容</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="194"/>
+        <location filename="../ui_NoDone.py" line="210"/>
+        <source>Comparison</source>
+        <translation>比較</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="195"/>
+        <location filename="../ui_NoDone.py" line="211"/>
+        <source>Title Only</source>
+        <translation>僅標題</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="196"/>
+        <location filename="../ui_NoDone.py" line="212"/>
+        <source>Blank</source>
+        <translation>空白</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="197"/>
+        <location filename="../ui_NoDone.py" line="213"/>
+        <source>Content with Caption</source>
+        <translation>帶標題的內容</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="198"/>
+        <location filename="../ui_NoDone.py" line="214"/>
+        <source>Pictrue with Caption</source>
+        <translation>帶標題的圖片</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="199"/>
+        <location filename="../ui_NoDone.py" line="215"/>
+        <source>Title and vertical text</source>
+        <translation>標題和豎排文本</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="200"/>
+        <location filename="../ui_NoDone.py" line="216"/>
+        <source>Vertical headings and text</source>
+        <translation>垂直標題和文本</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="202"/>
+        <source>text(.docx file or markdown)</source>
+        <translation>文字(.docx或markdown)</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="203"/>
+        <source>Change file</source>
+        <translation>選擇檔案</translation>
+    </message>
+    <message>
+        <source>Chage file</source>
+        <translation type="vanished">更改檔案</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="204"/>
+        <source>Creat</source>
+        <translation>創造</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="205"/>
+        <source>text style</source>
+        <translation>文本樣式</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="218"/>
+        <source>Help</source>
+        <translation>説明</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="219"/>
+        <source>Edit</source>
+        <translation>編輯</translation>
+    </message>
+    <message>
+        <location filename="../ui_NoDone.py" line="220"/>
+        <source>Language</source>
+        <translation>語言</translation>
+    </message>
+</context>
+<context>
+    <name>MessageBox</name>
+    <message>
+        <location filename="../run.py" line="45"/>
+        <location filename="../run.py" line="67"/>
+        <location filename="../run.py" line="85"/>
+        <location filename="../run.py" line="125"/>
+        <source>Error</source>
+        <translation>錯誤</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="45"/>
+        <location filename="../run.py" line="125"/>
+        <source>Error:&lt;br/&gt;%s&lt;br/&gt;&lt;br/&gt;&lt;a href=https://github.com/Simple-PPT/Simple-PPT/issues/new&gt;Feedback Error&lt;/a&gt;</source>
+        <translation>錯誤:&lt;br/&gt;%s&lt;br/&gt;&lt;br/&gt;&lt;a href=https://github.com/Simple-PPT/Simple-PPT/issues/new&gt;反饋錯誤&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="48"/>
+        <source>New version</source>
+        <translation>新版本</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="48"/>
+        <source>Find a new version:%s.
+Would you like to update?</source>
+        <translation>找到新版本：%s. 
+是否要更新？</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="55"/>
+        <location filename="../run.py" line="73"/>
+        <source>Download time out</source>
+        <translation>下載超時</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="55"/>
+        <location filename="../run.py" line="73"/>
+        <source>Download time out.&lt;/br&gt;Please try again.</source>
+        <translation>下載超時。&lt;/br&gt;請重試。</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="67"/>
+        <location filename="../run.py" line="85"/>
+        <source>Error:&lt;br/&gt;We don&apos;t know why, error code %s.&lt;br/&gt;&lt;br/&gt;&lt;a href=https://github.com/Simple-PPT/Simple-PPT/issues/new&gt;Feedback Error&lt;/a&gt;</source>
+        <translation>錯誤：&lt;br/&gt;我們不知道為什麼，錯誤代碼%s。&lt;br/&gt;&lt;br/&gt;&lt;a href=https://github.com/Simple-PPT/Simple-PPT/issues/new&gt;反饋錯誤&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="87"/>
+        <source>No new version</source>
+        <translation>沒有新版本</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="87"/>
+        <source>Your version is latest.</source>
+        <translation>您的版本是最新版本。</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="104"/>
+        <source>wrong file path</source>
+        <translation>錯誤的檔案路徑</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="104"/>
+        <source>The file&apos;s path is wrong!</source>
+        <translation>檔的路徑錯誤！</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="123"/>
+        <source>PPT ready</source>
+        <translation>PPT準備就緒</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="123"/>
+        <source>Your PPT is ready!
+It&apos;s in %s/ppts/%s</source>
+        <translation>您的PPT已準備就緒！
+它在 %s/ppts/%s</translation>
+    </message>
+</context>
+<context>
+    <name>QFileDialog</name>
+    <message>
+        <location filename="../run.py" line="89"/>
+        <source>Open a file</source>
+        <translation>打開檔</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="89"/>
+        <source>Documentation(*.docx *.md)</source>
+        <translation>文件 (*.docx *.md)</translation>
+    </message>
+</context>
+<context>
+    <name>about</name>
+    <message>
+        <location filename="../run.py" line="33"/>
+        <source>&lt;h2&gt;Simple PPT&lt;/h2&gt;version:0.0.2&lt;br/&gt;It can make a PPT for you.&lt;br/&gt;More see:&lt;a href=https://github.com/Simple-PPT/Simple-PPT&gt;Github&lt;/a&gt;</source>
+        <translation>&lt;h2&gt;簡單的PPT&lt;/h2&gt;版本：0.0.2&lt;br/&gt;它可以為您製作PPT。&lt;br/&gt;更多看:&lt;a href=https://github.com/Simple-PPT/Simple-PPT&gt;Github&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../run.py" line="33"/>
+        <source>About Simple PPT 0.1</source>
+        <translation>關於Simple PPT 0.1</translation>
+    </message>
+</context>
+</TS>
